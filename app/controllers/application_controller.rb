6dffc01fdf06443e3f8c1e9dc363ed0493142d77
Josh Cesana <joshuacesana@gmail.com>
2018-08-07 20:27:50 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,18 +8,12 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/' do
-    erb :index 
-  end
-  
   get '/posts/new' do 
     erb :new 
   end 
   
   post '/posts' do 
-    binding.pry 
-    @post = Post.new(params[:name], params[:content])
-    binding.pry 
-    erb :index 
+    @post = Post.create(params)
+    erb :posts 
   end 
 end
