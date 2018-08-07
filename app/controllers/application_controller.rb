b701edb5c118abac5fad9a1d4a5081d4dd2f8b9d
Josh Cesana <joshuacesana@gmail.com>
2018-08-07 20:39:49 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,7 +13,7 @@ class ApplicationController < Sinatra::Base
   end 
   
   post '/posts' do 
-    @post = Post.create(params)
+    @post = Post.create(params[:name], params[:content])
     redirect to '/posts' 
   end 
   
